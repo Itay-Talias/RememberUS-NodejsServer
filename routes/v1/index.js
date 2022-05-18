75c404c403ff7504afb187b320a79acda50db9c3
ItayTalias <itay.talias@gmail.com>
2022-05-18 15:39:38 +0300

    .

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const serverPythonRouter = require(`${__dirname}/serverPython.route`);
-
+ 
 
 const router = express.Router();
 router.use('/python', serverPythonRouter);
--- routes/v1/serverPython.route.js
+++ routes/v1/serverPython.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const axios = require('axios');
-const { path } = require('express/lib/application');
+const path = require('path');
 
 const router = express.Router();
 
@@ -12,18 +12,17 @@ function base64_encode(file) {
 router.get('/', (req, res) => {
     res.send("Hello1");
   });
-
+ 
 router.get('/send_photo_to_python_server', (req, res) => {
-    // axios.post('http://localhost:5000/image',
-    // { Base64_image : base64_encode('../../../public/di.jpg') } )
-    //   .then(response => {
-    //     console.log(response.data);
-    //     res.send(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
-    res.send("Hello");
+    axios.post('http://localhost:5000/image',
+    { Base64_image : base64_encode(path.resolve(`${__dirname}/../../public/di.jpg`)) } )
+      .then(response => {
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   });
 
   module.exports = router
