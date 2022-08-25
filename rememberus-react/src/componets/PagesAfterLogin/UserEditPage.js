4b94ecaf5b2184133f2034aa513c7751c9dbb194
eladtennenholtz <eladtennenholtz1@gmail.com>
2022-08-25 08:17:07 +0300

    eladChangePassword

--- rememberus-react/src/componets/PagesAfterLogin/UserEditPage.js
+++ rememberus-react/src/componets/PagesAfterLogin/UserEditPage.js
@@ -1,40 +1,62 @@
 import { React, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+//import "./UserEditPage.css";
 
 import "./background-style.css";
 
 const UserEditPage = (props) => {
-    const [enteredUserName, setEnteredUserName] = useState("");
-    const UserNameChangeHandler = (event) => {
-        setEnteredUserName(event.target.value);
-    };
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const UserNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+  const PasswordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
 
-    const ChangeDetailsHandler = () => {
-        if (enteredUserName !== "") {
-            const newUserName = {
-                userName: "itay", //props.userInfo.userName,
-                updateField: "userName",
-                updateData: enteredUserName,
-            };
-            console.log(newUserName);
-        }
-    };
-    return (
-        <div className={props.display ? "continer" : "display-none"}>
-            <div>
-                <TextField
-                    className="Username"
-                    label="Username"
-                    value={enteredUserName}
-                    onChange={UserNameChangeHandler}
-                />
-                <Button variant="contained" onClick={ChangeDetailsHandler}>
-                    Change Details
-                </Button>
-            </div>
-        </div>
-    );
+  const ChangeDetailsHandler = () => {
+    if (enteredUserName !== "") {
+      const newUserName = {
+        userName: "itay", //props.userInfo.userName,
+        updateField: "userName",
+        updateData: enteredUserName,
+      };
+      console.log(newUserName);
+    }
+    if (enteredPassword !== "") {
+      const newPassword = {
+        password: "123456", //props.userInfo.userName,
+        updateField: "password",
+        updateData: enteredPassword,
+      };
+      console.log(newPassword);
+    }
+  };
+  return (
+    <div className={props.display ? "continer" : "display-none"}>
+      <div>
+        <TextField
+          className="Username"
+          label="Username"
+          value={enteredUserName}
+          onChange={UserNameChangeHandler}
+        />
+
+        <Button variant="contained" onClick={ChangeDetailsHandler}>
+          Change Details
+        </Button>
+      </div>
+      <div>
+        <TextField
+          className="Password"
+          label="Password"
+          value={enteredPassword}
+          onChange={PasswordChangeHandler}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default UserEditPage;
